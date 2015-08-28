4b0c8b4bebdb905d614b460b052205f4accde1b6
jcrump <jcrump@tulane.edu>
2015-08-28 13:46:08 -0500

    no message

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -78,7 +78,7 @@ url(<?php print $path; ?>/fonts/CharisSIL/CharisSIL-BI.svg#charis_silbold_italic
 
  
 <?php
-print "<link rel='stylesheet' href='" . $path . "/style/microsite.css?v=2' type='text/css'>";
+print "<link rel='stylesheet' href='" . $path . "/style/microsite.css?v=3' type='text/css'>";
 print "<script type='text/javascript' src='" . $path . "/js/jquery.js?ver=1.11.2'></script>";
 print "<script type='text/javascript' src='" . $path . "/js/jquery-migrate.min.js?ver=1.2.1'></script>";
 print "<script type='text/javascript' src='" . $path . "/js/global.js?ver=1.0.0'></script>";
