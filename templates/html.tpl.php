7888b650c01c268bc60140f1aaced7b242ad4569
jcrump <jcrump@tulane.edu>
2015-09-23 15:31:33 -0500

    no message

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -100,7 +100,15 @@ print "<script type='text/javascript' src='" . $path . "/js/foundation.min.js'><
 </head>
 
 <body class="home blog custom-header header-image header-full-width full-width-content cafe-pro-home" itemscope="itemscope" itemtype="http://schema.org/WebPage">
-
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-5D8S6D"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5D8S6D');</script>
+<!-- End Google Tag Manager -->
 <?php
 
 
@@ -121,15 +129,7 @@ print $page_top; ?>
 </div>
 
 
-<!-- Google Tag Manager -->
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-5D8S6D"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5D8S6D');</script>
-<!-- End Google Tag Manager -->
+
 
 
 </body>
