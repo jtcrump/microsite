341e286a59b7287db9786577c1a537f8ee895e15
jcrump <jcrump@tulane.edu>
2015-09-24 15:26:50 -0500

    no message

--- templates/node.tpl.php
+++ templates/node.tpl.php
@@ -297,9 +297,9 @@ $i3++;
 
 <?php
 
-
+print "<div id=\"front-page-4\" class=\"front-page-4 image-section\">";
     print drupal_render(drupal_get_form('rsvp_responses_form')); 
-
+print "</div>";
 
 if(strlen(@$content['field_page_5_content'][0]['#markup']) > 5){
 ?>
