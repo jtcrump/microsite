a8afc0f0337412848503ab9a65d9d455ce1ffabf
jcrump <jcrump@tulane.edu>
2015-10-20 15:37:00 -0500

    updated default theme

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -11,9 +11,13 @@ print render($page['content']);
   <header role="banner" class="l-header">
 
     <?php if ($top_bar): ?>
+
+
+
+
+
+
 <div id="header" role="banner" class="header">
-
-
 	<div class="row-top-nav">
 		<div class="topbar container">
       			<div class="col-md-6">
@@ -23,117 +27,67 @@ print render($page['content']);
              		<li><a href="javascript:;"><i class="fa fa-folder-open"></i> MyTulane</a></li>
           		</ul>
       			</div>
-      			<div class="col-md-6">
-                        <!---
-          			<div class="search-container">
-            			<form action="#" method="post" class="inline-form searchbox">
-            			<input type="search" placeholder="Search......" name="search" class="searchbox-input" onkeyup="buttonUp();" required>
-            			<input type="submit" class="searchbox-submit" value="GO">
-            			<span class="searchbox-icon"><i class="fa fa-lg fa-search"></i><span class="sr-only">Search</span></span>
-            			</form>
-          			</div>     
---->
-                                
+      			<div class="col-md-6">        
 <div class="search-container">
 <!-- Google CSE Search Box Begins --->
 <form action="//tulane.edu/search-results2.cfm" id="searchbox_017967096760080950669:bl9vlsogwoo" name="Site2" class="inline-form searchbox">
 <input type="hidden" name="cx" value="017967096760080950669:bl9vlsogwoo" />
 <input type="hidden" name="cof" value="FORID:11" />
 <input type="hidden" name="as_dt" value="" />
- 
 <input type="hidden" name="as_sitesearch" value="tulane.edu#getDirectoryFromPath(cgi.script_name)#" />
 <input type="hidden" name="siteURL" value="tulane.edu" />
-<input type="search" placeholder="Search......" name="q" class="searchbox-input" onkeyup="buttonUp();" required>
-			
-<input id="go" type="submit" name="sa" class="searchbox-submit" value="GO" placeholder="GO"/>
+<ul class="search">
+<li>
+<input type="search" placeholder="Search......" name="q" class="searchbox-input" onkeyup="buttonUp();" required />
+</li>
+<li>			
+<input id="go" type="submit" name="sa" class="searchbox-submit" value="GO" placeholder="GO" />
+</li>
+</ul>
 <!--- <span class="searchbox-icon"><i class="fa fa-lg fa-search"></i><span class="sr-only">Search</span></span> --->
 </form>
 </div>
 <script type="text/javascript" src="http://www.google.com/coop/cse/brand?form=searchbox_017967096760080950669%3Abl9vlsogwoo"></script>
 					<!--- Google CSE Search Box Ends --->
-
-  
 			</div>
 		</div>
 	</div>
 
+
+
+
+
 	<div class="row-banner">
 		<div class="container">
 			<div class="header-table">
-
-
-
 <ul id="header-mid-banner">
-
 <li><div class="cell-shield"><div class="tu-shield"></div></div></li>
-
 <li><div class="cell-school">
-<a href="//tulane.edu"><div class="main_title">T<span class="move">ulane University</span></div></a>
+&nbsp;
 </div></li>
-
 <li><div class="cell-dept"><h2><?php print $linked_site_name; ?><h2></div></li>
-
 </ul>
-
-
- 
-
 			</div> 
 		</div>
 	</div>   
- 
-
-      <?php if ($alt_main_menu): ?>
-        <nav id="main-menu" class="navigation white" role="navigation">
 
 
 
 
 
 
-<div class="main_menu_wrapper">
-	<label for="show-menu" class="show-menu">Show Menu</label>
-	<input type="checkbox" id="show-menu" role="button">
-<?php print ($alt_main_menu); ?></div>
-        </nav>  <!--- /#main-menu --->
-      <?php endif; ?>
-</div>
-
-<div class="secondary_menu_wrapper">
-      <?php if ($alt_secondary_menu): ?>
-        <nav id="secondary-menu" class="navigation" role="navigation">
-          <?php print $alt_secondary_menu; ?>
-        </nav>  <!--- /#secondary-menu --->
-      <?php endif; ?>
-</div>
 
 
 
-
-
-      <!--.top-bar 
+<!--- menu goes here --->
+      <!--.top-bar -->
       <?php if ($top_bar_classes): ?>
       <div class="<?php print $top_bar_classes; ?>">
       <?php endif; ?>
-
         <nav class="top-bar" data-topbar <?php print $top_bar_options; ?>>
-
           <ul class="title-area">
-<li>
-<a class="top-fixed-nav" href="http://tulane.edu/email/">Email</a> 
-<a class="top-fixed-nav" href="https://gibson.tulane.edu/">Gibson Online</a> 
-<a class="top-fixed-nav" href="http://tulane.blackboard.com/">myTulane</a>
-</li>
-            <li class="name"><h1><a href="/">Tulane
-
-<?php print $linked_site_name; ?>
-
-</h1></li>
-
-
             <li class="toggle-topbar menu-icon"><a href="#"><span><?php print $top_bar_menu_text; ?></span></a></li>
           </ul>
-
           <section class="top-bar-section">
             <?php if ($top_bar_main_menu) :?>
               <?php print $top_bar_main_menu; ?>
@@ -143,51 +97,36 @@ print render($page['content']);
             <?php endif; ?>
           </section>
         </nav>
-      <?php if ($top_bar_classes): ?>
+      <?php if ($top_bar_classes): ?><br /><br /><br /><br /><br />
       </div>
       <?php endif; ?>
-      /.top-bar 
+      <!--/.top-bar -->
+
+
+
+
+
     <?php endif; ?>
 
-     Title, slogan and menu 
-    <?php if ($alt_header): ?>
-    <section class="row <?php print $alt_header_classes; ?>">
 
-      <?php if ($linked_logo): print $linked_logo; endif; ?>
 
-      <?php if ($site_name): ?>
-        <?php if ($title): ?>
-          <div id="site-name" class="element-invisible">
-            <strong>
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-            </strong>
-          </div>
-        <?php else: /* Use h1 when the content title is empty */ ?>
-          <h1 id="site-name">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-          </h1>
-        <?php endif; ?>
-      <?php endif; ?>
 
-      <?php if ($site_slogan): ?>
-        <h2 title="<?php print $site_slogan; ?>" class="site-slogan"><?php print $site_slogan; ?></h2>
-      <?php endif; ?>
 
-      <?php if ($alt_main_menu): ?>
-        <nav id="main-menu" class="navigation" role="navigation">
-          <?php print ($alt_main_menu); ?>
-        </nav>  /#main-menu 
-      <?php endif; ?>
 
-      <?php if ($alt_secondary_menu): ?>
-        <nav id="secondary-menu" class="navigation" role="navigation">
-          <?php print $alt_secondary_menu; ?>
-        </nav>  /#secondary-menu 
-      <?php endif; ?>
 
-    </section>
-    <?php endif; ?>
-     End title, slogan and menu -->
+
+    
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -331,16 +270,22 @@ print render($page['content']);
   <!--.l-footer-->
   <footer class="l-footer panel row" role="contentinfo">
 
-
-
-<div class="footer large-12 columns">
-<?php if (!empty($page['footer'])): ?>
-<?php  print render($page['footer']); ?>
-<?php endif; ?>
+      <div class="footer large-12 columns">
+<div class="cell-school">
+<a href="//tulane.edu"><div class="main_title">T<span class="move">ulane University</span></div></a>
 </div>
+    <?php if (!empty($page['footer'])): ?>
+        <?php print render($page['footer']); ?>
+    <?php endif; ?>
+      </div>
 
-
-
+<!--- 
+    <?php if ($site_name) :?>
+      <div class="copyright large-12 columns">
+       &copy; <?php print date('Y') . ' ' . check_plain($site_name) . ' ' . t('All rights reserved.'); ?> 
+      </div>
+    <?php endif; ?>
+--->
   </footer>
   <!--/.footer-->
 
